ad4c5f6d5e077c764a4e9d141a3cea209d5cc772
opet <opet@Academico.edu>
2018-09-06 21:40:54 -0300

    Projeto onde foi visto MVC,responsabilidade das camadas, rotas,layout,views, partial views, view fortemente tipadas,inferencia de tipos

---
+++ src/ToDoMvc/Controllers/ToDoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoMvc.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new Models.View.ToDoViewModel()
+            {
+                Items = new List<Models.ToDoItem>()
+
+
+                {
+                    new Models.ToDoItem{
+                    Id = Guid.NewGuid(),
+                    Title = "Item 1",
+                    DueAt = DateTime.Now
+                    }
+
+                    new Models.ToDoItem{
+                    Id = Guid.NewGuid(),
+                    Title = "Item 2",
+                    DueAt = DateTime.Now
+                    }
+
+                    new Models.ToDoItem{
+                    Id = Guid.NewGuid(),
+                    Title = "Item 3",
+                    DueAt = DateTime.Now
+                    }
+                }
+
+
+            };
+
+            return View(vm);
+        }
+    }
+}
